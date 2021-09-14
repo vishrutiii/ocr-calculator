85463f86831911cdc3a49cae92ae0f70ebb56adb
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-09-14 13:41:27 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+import cv2
+import tessaract
+import tkinter
+
+from read_image import read_image
---
+++ utils/read_image.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+import cv2
+
+def read_image(image_path):
+    # complete the function
+    pass
