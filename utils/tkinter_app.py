a690a9e33fa091621fa9be85f0b51ac3c469cebd
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-09-15 20:14:58 +0530

    create tkinter app
    In this code, I have create an app which will capture the image for our calculator.

--- utils/tkinter_app.py
+++ utils/tkinter_app.py
@@ -5,7 +5,7 @@ from tkinter.filedialog import askopenfilename
 import os
 import PIL
 from PIL import ImageTk
-
+import cv2
 
 # function to show output
 def showOutput():
@@ -30,9 +30,43 @@ def openImage():
         image_.image = img
         image_.place(x=97,y=78)
 
+
+
+# open the web camera
+def openCamera():
+    video = cv2.VideoCapture(0)
+
+    def show_frame():
+        ret, frame = video.read()
+        cv2image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        img = PIL.Image.fromarray(cv2image)
+        img = img.resize((canvas_width, canvas_height))
+        imgtk = ImageTk.PhotoImage(image = img)
+        v = Label(image=imgtk)
+        v.image = imgtk
+        v.place(x=97,y=78)
+        v.after(10, show_frame)
+
+    show_frame()
+
+    def get_frame():
+        if video.isopened():
+            ret, frame = video.read()
+            if ret:
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            else:
+                return (ret, None)
+        else:
+            return (None, None)
+
+
+# take the snapshot of that image
+def takeSnap():
+    pass
+
 root = Tk()
 root.geometry("600x800")
-root.minsize(500,600)
+root.minsize(600,800)
 root.maxsize(600,800)
 root.title("OCR-Calculator")
 
@@ -42,6 +76,7 @@ title_window.pack(fill = Y,padx = 20, pady = 20)
 canvas_width = 400
 canvas_height = 300
 tkinter_canvas = Canvas(root, width = canvas_width, height = canvas_height, bg="white")
+# tkinter_canvas = Frame(root, width=canvas_width, height=canvas_height)
 tkinter_canvas.pack()
 
 
@@ -50,10 +85,10 @@ webcam_frame = Frame(root)
 webcam_frame.pack(side=TOP)
 select_button = Button(webcam_frame, borderwidth=7, bg="black", fg="white", text="Select From Computer", font="comicsansms 13", command=openImage)
 select_button.pack(side=LEFT,anchor='ne', pady=20, padx=35)
-snapshot = Button(webcam_frame, borderwidth=7, bg="black", fg="white", text="SnapShot", font="comicsansms 13")
+snapshot = Button(webcam_frame, borderwidth=7, bg="black", fg="white", text="SnapShot", font="comicsansms 13", command=takeSnap)
 snapshot.pack(side = BOTTOM, anchor='ne', padx=35)
 
-webcam_button = Button(webcam_frame, borderwidth=7, bg="black", fg="white", text="Open WebCam", font="comicsansms 13")
+webcam_button = Button(webcam_frame, borderwidth=7, bg="black", fg="white", text="Open WebCam", font="comicsansms 13", command=openCamera)
 webcam_button.pack(side=LEFT,anchor='ne', pady=20,padx=35)
 
 input_frame = Frame(root,)
