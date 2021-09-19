451fb1c59b3308a77cbd935c1d075a4dffdb33cb
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-09-19 15:34:08 +0530

    added code to get the output

--- utils/solver.py
+++ utils/solver.py
@@ -2,10 +2,23 @@ import os
 import sys
 import time
 import math
+from cv2 import TermCriteria_COUNT
+import pandas as pd
+import numpy as np
+
+filepath = "A:\\Pending Projects\\OCR Calculator\\ocr-calculator\\Data\\fun.txt"
+
+df = pd.read_table(filepath, sep=", ")
+input_fun = np.array(df['Input'])
+output_fun = np.array(df['Output'])
 
 
 def get_output(input_text : str = ""):
     """
     It will take input as a string and return the output of the given input mathematics problem
     """
-    return 1
+    if input_text in input_fun:
+        return output_fun[np.where(input_fun == input_text)]
+    
+    else :
+        raise ValueError("Required Input Function Not Found")
