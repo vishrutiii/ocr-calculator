0df115d1ed10fd7c596b27f181b35e8d8834a84e
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-09-19 15:33:29 +0530

    load image using cv2/pil

--- utils/read_image.py
+++ utils/read_image.py
@@ -2,18 +2,25 @@ import os
 import sys
 
 import cv2
-
-def read_image(image_path, show = True):
+import PIL
+from PIL import Image
+import numpy as np
+def read_image(image_path, show = True, module : str = "" ):
     # complete the function
-    image = cv2.imread(image_path)
-    try:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image
-    except : 
-        return image
-    finally :
+    if module == "opencv":
+        image = cv2.imread(image_path)
+        try:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            return image
+        except : 
+            return image
+        finally :
+            if show:
+                cv2.imshow("Image",image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    else:
+        image = Image.open(image_path)
         if show:
-            cv2.imshow("Image",image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    
+            image.show()
+        return np.array(image)
