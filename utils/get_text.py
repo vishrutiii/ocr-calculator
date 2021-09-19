d5d2bcadae3ad933b87ee486983aa5406dc7ae76
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-09-19 15:32:45 +0530

    add tesseract code

--- utils/get_text.py
+++ utils/get_text.py
@@ -2,21 +2,25 @@ import os
 import sys
 import math
 import time
+from numpy.lib.type_check import imag
 import pytesseract
 import PIL
 from PIL import Image
 from read_image import read_image
 import numpy as np
+import cv2
 
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\dev24\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
 
 def get_text(filename : str = ""):
     """
     It will take the input filename, and extract the input the maths text from it.
     """
-    # image = read_image(filename)
-    img = Image.open(filename)
-    print(pytesseract.image_to_string(img))
-    return img
+    image = read_image(filename, show=False, module="PIL")
 
-path = "A:\Pending Projects\OCR Calculator\ocr-calculator\Images\/abc.png"
-get_text(path)
+    text = pytesseract.image_to_string(image)
+    return (text, image)
+
+# file = "A:\\Pending Projects\\OCR Calculator\\ocr-calculator\\Images\\tri.png"
+# t,_ = get_text(file)
+# print(t)
